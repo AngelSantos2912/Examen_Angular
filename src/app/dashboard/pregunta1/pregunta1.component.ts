dcaccfa0a94642f4d4eee2ca4f031b379e801327
AngelSantos2912 <angelleonardo975533406@gmail.com>
2024-06-22 13:56:00 -0500

    ya aca la dejo

--- src/app/dashboard/pregunta1/pregunta1.component.ts
+++ src/app/dashboard/pregunta1/pregunta1.component.ts
@@ -9,4 +9,43 @@ import { Component } from '@angular/core';
 })
 export class Pregunta1Component {
 
+
+  resultado = "esperando resultados"
+
+  mostrarmensaje = false
+
+
+  calcular = {
+
+    añosTrabajados: null,
+    porcentajeUtilidad: null,
+  }
+
+
+
+  calcularUtilidades(salarioMensual: number, añosTrabajados: number): number {
+    let porcentajeUtilidad: number;
+
+    if (añosTrabajados < 1) {
+        porcentajeUtilidad = 0.05 * salarioMensual;
+    } else if (añosTrabajados >= 1 && añosTrabajados < 2) {
+        porcentajeUtilidad = 0.07;
+    } else if (añosTrabajados >= 2 && añosTrabajados < 5) {
+        porcentajeUtilidad = 0.10;
+    } else if (añosTrabajados >= 5 && añosTrabajados < 10) {
+        porcentajeUtilidad = 0.15;
+    } else {
+        porcentajeUtilidad = 0.20;
+    }
+
+    return salarioMensual * porcentajeUtilidad;
+
+
+    this.mostrarmensaje = true
+
+    this.resultado = "Su tiempo de trabajo es: " + añosTrabajados.toFixed(2).toString() + ", " + "y su valor por utilidades es: " + porcentajeUtilidad
+
 }
+
+}
+
